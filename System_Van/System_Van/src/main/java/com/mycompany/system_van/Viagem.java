9a9e02c70c51e978b941b10bc7f3e3c7fcffbc17
Alf0i <iagoalfaya@gmail.com>
2022-12-04 20:10:40 -0300

    Create Viagem.java

---
+++ System_Van/System_Van/src/main/java/com/mycompany/system_van/Viagem.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.system_van;
+
+/**
+ *
+ * @author aluno.cyber
+ */
+public class Viagem {
+// saber quem é o motorista que será responsável pela viagem, bem como o gerente | cidade de partida e a cidade de destino
+    String gerente;
+    String motorista;
+    String partida;
+    String destino;
+    int id;
+    
+    
+    public Viagem(){
+    }
+    
+    
+    public Viagem(String motorista, String gerente, String partida, String destino, int id){
+        this.gerente = gerente;
+        this.motorista = motorista;
+        this.destino = destino;
+        this.partida = partida;
+        this.id = id;
+    }
+
+    
+    public String getPartida() {
+        return partida;
+    }
+    public void setPartida(String partida) {
+        this.partida = partida;
+    }
+
+    
+    public String getDestino() {
+        return destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    
+    public String getGerente() {
+        return gerente;
+    }
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
+    
+    public String getMotorista() {
+        return motorista;
+    }
+    public void setMotorista(String motorista) {
+        this.motorista = motorista;
+    } 
+}
