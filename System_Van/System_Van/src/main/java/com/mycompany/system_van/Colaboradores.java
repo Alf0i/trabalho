f68c85e1b47bdcb52511f730a1c11e91d06ed917
Alf0i <iagoalfaya@gmail.com>
2022-12-05 08:28:36 -0300

    Create Colaboradores.java

---
+++ System_Van/System_Van/src/main/java/com/mycompany/system_van/Colaboradores.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.system_van;
+
+/**
+ *
+ * @author iagoa
+ */
+public class Colaboradores {
+    String gerente;
+    String motorista;
+}
+
